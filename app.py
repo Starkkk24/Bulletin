828fef9ec8626f184b420fff45974379f2a12a37
Stark <ankit566kota@gmail.com>
2025-07-24 21:11:41 +0530

    cmit-3

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template
 
-app = __name__
+app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True,port=8070)
+    app.run(debug=True,port=8070 )
