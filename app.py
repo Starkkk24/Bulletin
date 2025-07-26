a1fef5ce054ad4669179e0c2645903e1afc52c26
Stark <ankit566kota@gmail.com>
2025-07-26 23:49:05 +0530

    cmit-39 (created signup page)

--- app.py
+++ app.py
@@ -10,5 +10,9 @@ def home():
 def login():
     return render_template('login.html')
 
+@app.route('/signup')
+def signup():
+    return render_template("signup.html")
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8070 )
